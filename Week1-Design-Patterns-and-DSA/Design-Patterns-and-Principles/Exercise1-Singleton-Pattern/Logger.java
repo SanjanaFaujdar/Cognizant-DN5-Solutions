23290eb3edcdc23cc8e6af479f000fa4f379ff3a
SanjanaFaujdar <sanjanafaujdar589@gmail.com>
2026-06-24 10:25:57 +0530

    Implement Singleton pattern in Logger class

---
+++ Week1-Design-Patterns-and-DSA/Design-Patterns-and-Principles/Exercise1-Singleton-Pattern/Logger.java
@@ -0,0 +1,23 @@
+public class Logger {
+    private static Logger instance;
+
+    private Logger() {}
+
+    public static Logger getInstance() {
+        if (instance == null) {
+            instance = new Logger();
+        }
+        return instance;
+    }
+
+    public void log(String message) {
+        System.out.println("LOG: " + message);
+    }
+
+    public static void main(String[] args) {
+        Logger logger1 = Logger.getInstance();
+        Logger logger2 = Logger.getInstance();
+        logger1.log("Singleton Logger initialized");
+        System.out.println("Same instance? " + (logger1 == logger2));
+    }
+}
