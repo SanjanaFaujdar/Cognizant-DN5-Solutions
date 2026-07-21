3adfe9eebfbc0f8bd7e4b1d4944d634af08b62de
SanjanaFaujdar <sanjanafaujdar589@gmail.com>
2026-07-21 17:45:39 +0530

    Add OrmLearnApplication with country service methods

---
+++ Week3-Spring-Core-JPA-REST/Spring-Data-JPA-Hibernate/spring-data-jpa-handson-1/src/main/java/com/cognizant/ormlearn/OrmLearnApplication.java
@@ -0,0 +1,56 @@
+package com.cognizant.ormlearn;
+
+import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import com.cognizant.ormlearn.model.Country;
+import com.cognizant.ormlearn.service.CountryService;
+
+@SpringBootApplication
+public class OrmLearnApplication {
+
+    private static final Logger LOGGER =
+        LoggerFactory.getLogger(OrmLearnApplication.class);
+
+    private static CountryService countryService;
+
+    public static void main(String[] args) {
+        ApplicationContext context =
+            SpringApplication.run(OrmLearnApplication.class, args);
+        countryService = context.getBean(CountryService.class);
+        LOGGER.info("Inside main");
+
+        testGetAllCountries();
+        testFindCountryByCode();
+        testAddCountry();
+    }
+
+    // Handson 1: Get all countries
+    private static void testGetAllCountries() {
+        LOGGER.info("Start");
+        List<Country> countries = countryService.getAllCountries();
+        LOGGER.debug("countries={}", countries);
+        LOGGER.info("End");
+    }
+
+    // Handson 6: Find by code
+    private static void testFindCountryByCode() {
+        LOGGER.info("Start");
+        Country country = countryService.findCountryByCode("IN");
+        LOGGER.debug("Country:{}", country);
+        LOGGER.info("End");
+    }
+
+    // Handson 7: Add new country
+    private static void testAddCountry() {
+        LOGGER.info("Start");
+        Country country = new Country();
+        country.setCode("XX");
+        country.setName("Test Country");
+        countryService.addCountry(country);
+        LOGGER.info("End");
+    }
+}
