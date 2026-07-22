67747d201af6f6155eb1bc316a88dc2ae384a9c5
SanjanaFaujdar <sanjanafaujdar589@gmail.com>
2026-07-22 17:46:50 +0530

    Create CohortDetails.js

---
+++ Week4-Microservices-and-React/ReactJS/ReactJS-HOL-5/src/CohortDetails.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import styles from './CohortDetails.module.css';
+
+class CohortDetails extends Component {
+  render() {
+    const { name, status, startDate, endDate } = this.props;
+    const headingStyle = {
+      color: status === 'ongoing' ? 'green' : 'blue'
+    };
+
+    return (
+      <div className={styles.box}>
+        <h3 style={headingStyle}>{name}</h3>
+        <dl>
+          <dt>Status</dt>
+          <dd>{status}</dd>
+          <dt>Start Date</dt>
+          <dd>{startDate}</dd>
+          <dt>End Date</dt>
+          <dd>{endDate}</dd>
+        </dl>
+      </div>
+    );
+  }
+}
+
+export default CohortDetails;
