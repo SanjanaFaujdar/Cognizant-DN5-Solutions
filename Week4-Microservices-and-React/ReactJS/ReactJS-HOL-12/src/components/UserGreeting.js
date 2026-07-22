97033178afc9a3569886f55c7de7de17d769bacd
SanjanaFaujdar <sanjanafaujdar589@gmail.com>
2026-07-22 22:16:33 +0530

    Add UserGreeting component for user welcome message

---
+++ Week4-Microservices-and-React/ReactJS/ReactJS-HOL-12/src/components/UserGreeting.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function UserGreeting() {
+  return (
+    <div>
+      <h2>Welcome back!</h2>
+      <p>You can now book your tickets.</p>
+    </div>
+  );
+}
+
+export default UserGreeting;
