b6fdc5a3d05ae9debd6e08270270365c469ffff7
SanjanaFaujdar <sanjanafaujdar589@gmail.com>
2026-07-22 18:10:46 +0530

    Create Counter.js

---
+++ Week4-Microservices-and-React/ReactJS/ReactJS-HOL-11/src/components/Counter.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 0 };
+  }
+
+  increment = () => {
+    this.setState({ count: this.state.count + 1 });
+    alert("Hello followed by a static message");
+  }
+
+  decrement = () => {
+    this.setState({ count: this.state.count - 1 });
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Counter: {this.state.count}</h2>
+        <button onClick={this.increment}>Increment</button>
+        <button onClick={this.decrement}>Decrement</button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
