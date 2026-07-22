77e010bf022def6035b29f4d657dff5d38ab1241
SanjanaFaujdar <sanjanafaujdar589@gmail.com>
2026-07-22 22:15:58 +0530

    Add GuestGreeting component for user sign-up prompt

---
+++ Week4-Microservices-and-React/ReactJS/ReactJS-HOL-12/src/components/GuestGreeting.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function GuestGreeting() {
+  return (
+    <div>
+      <h2>Please sign up.</h2>
+      <p>Browse our available flights and sign up to book tickets.</p>
+    </div>
+  );
+}
+
+export default GuestGreeting;
