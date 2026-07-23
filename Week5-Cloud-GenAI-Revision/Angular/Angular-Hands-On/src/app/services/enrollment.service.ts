5acbbaa91372727ccf09c52cd5588fa7b35c0355
SanjanaFaujdar <sanjanafaujdar589@gmail.com>
2026-07-23 16:50:21 +0530

    Add EnrollmentService for course management

---
+++ Week5-Cloud-GenAI-Revision/Angular/Angular-Hands-On/src/app/services/enrollment.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EnrollmentService {
+
+  enrolledCourses: number[] = [];
+
+  enroll(courseId: number): void {
+    this.enrolledCourses.push(courseId);
+  }
+
+  unenroll(courseId: number): void {
+    this.enrolledCourses =
+      this.enrolledCourses.filter(id => id !== courseId);
+  }
+
+  isEnrolled(courseId: number): boolean {
+    return this.enrolledCourses.includes(courseId);
+  }
+
+  getEnrolledCourses(): number[] {
+    return this.enrolledCourses;
+  }
+}
